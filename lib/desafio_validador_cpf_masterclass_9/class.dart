9bf05dcef4a5c820c47322751556e063ed293a4e
Pedro Siqueira <pedro.siqueira1999@hotmail.com>
2023-08-10 20:33:43 -0300

    desafio cpf

---
+++ lib/desafio_validador_cpf_masterclass_9/class.dart
@@ -0,0 +1,45 @@
+
+
+class CPF {
+  final String cpf;
+
+  CPF({required this.cpf});
+
+  bool validador() {
+    var valideCPF = cpf.substring(12, 14);
+    var digitoCorreto = primeiroDigito() + segundoDigito();
+
+    return valideCPF == digitoCorreto;
+  }
+
+  String primeiroDigito() {
+    int multiplicador = 2;
+    int somatorio = 0;
+    var numeros =
+        cpf.substring(0, 11).replaceAll('.', '').split('').reversed.toList();
+
+    for (var numero in numeros) {
+      somatorio += int.parse(numero) * multiplicador++;
+    }
+
+    int resultado = somatorio % 11;
+    var digito1 = resultado >= 2 ? 11 - resultado : resultado = 0;
+    return digito1.toString();
+  }
+
+  String segundoDigito() {
+    int multiplicador = 2;
+    int somatorio = 0;
+    var numeros = cpf.substring(0, 11).replaceAll('.', '').split('');
+    numeros.add(primeiroDigito());
+    numeros.reversed.toList();
+
+    for (var numero in numeros) {
+      somatorio += int.parse(numero) * multiplicador++;
+    }
+
+    int resultado = somatorio % 11;
+    var digito2 = resultado >= 2 ? 11 - resultado : resultado = 0;
+    return digito2.toString();
+  }
+}
---
+++ lib/desafio_validador_cpf_masterclass_9/main.dart
@@ -0,0 +1,7 @@
+import 'class.dart';
+
+void main(List<String> arguments) {
+  var pessoa = CPF(cpf: '177.302.827-80');
+
+  print(pessoa.validador());
+}
